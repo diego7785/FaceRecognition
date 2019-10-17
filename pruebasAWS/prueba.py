631989695fc8874f2f8d85135c07e98da53da740
diego7785 <diego.viafara@correounivalle.edu.co>
2019-10-17 16:27:17 -0500

    Pruebas foto vs foto Rekognition API

---
+++ pruebasAWS/prueba.py
@@ -0,0 +1,75 @@
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+import time
+
+def compare_faces(sourceFile, targetFile):
+
+    client=boto3.client('rekognition')
+   
+    imageSource=open(sourceFile,'rb')
+    imageTarget=open(targetFile,'rb')
+
+    response=client.compare_faces(SimilarityThreshold=80,
+                                  SourceImage={'Bytes': imageSource.read()},
+                                  TargetImage={'Bytes': imageTarget.read()})
+    
+    for faceMatch in response['FaceMatches']:
+        position = faceMatch['Face']['BoundingBox']
+        similarity = str(faceMatch['Similarity'])
+        print('The face at ' +
+               str(position['Left']) + ' ' +
+               str(position['Top']) +
+               ' matches with ' + similarity + '% confidence')
+
+    imageSource.close()
+    imageTarget.close()     
+    return len(response['FaceMatches'])          
+
+def main():
+    start = time.time()
+    source_file='George.jpg'
+    target_file='George1.jpg'
+    face_matches=compare_faces(source_file, target_file)
+    end = time.time()
+    elapsedTime=end-start
+    print("Face matches: " + str(face_matches))
+    print('Time: '+str(elapsedTime))
+
+
+if __name__ == "__main__":
+    main()
+
+'''
+#PHOTO LABELS
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+
+def detect_labels_local_file(photo):
+
+
+    client=boto3.client('rekognition')
+   
+    with open(photo, 'rb') as image:
+        response = client.detect_labels(Image={'Bytes': image.read()})
+        
+    print('Detected labels in ' + photo)    
+    for label in response['Labels']:
+        print (label['Name'] + ' : ' + str(label['Confidence']))
+
+    return len(response['Labels'])
+
+def main():
+    photo='Diego.png'
+
+    label_count=detect_labels_local_file(photo)
+    print("Labels detected: " + str(label_count))
+
+
+if __name__ == "__main__":
+    main()
+
+'''
