8f9cdb0f44d5ea5e9f751fbd60ea9dd51c20d7b6
diego7785 <diego.viafara@correounivalle.edu.co>
2019-10-24 09:26:13 -0500

    Crear collection con GO

---
+++ awsGO/createCollection.go
@@ -0,0 +1,41 @@
+//Creating a collection with GO
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/rekognition"
+)
+
+var collectionName = "Prueba"
+func main(){
+	start := time.Now()
+	time.Sleep(time.Second*2)
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+
+	if err != nil {
+		fmt.Println("Ops: " + err.Error())
+		return
+	}
+
+	_rekognitionService := rekognition.New(sess)
+	input := &rekognition.CreateCollectionInput{
+		CollectionId: aws.String(collectionName),
+	}
+
+	result, err := _rekognitionService.CreateCollection(input)
+	if err != nil {
+		fmt.Println("Ops: ", err.Error())
+		return
+	}
+
+	fmt.Println(result)
+	elapsedTime := time.Since(start)
+	fmt.Println("Elapsed time: ", elapsedTime)
+}
